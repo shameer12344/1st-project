646210b25b0032ccaf13d17838b1a0cdff4967ec
shamer12 <skshahmeershah786@gmail.com>
2024-10-14 11:24:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Error! Please enter a valid password")
+alert("Welcome to JS land\nHappy coding!")
+alert("Welcome to JS land...")
+alert("Hello... i can run jS through my web browser console")
